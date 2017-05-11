6828a118fe12331881637a0d016ae87b1dbcb967
Alexandra Penberg <apenberg@movableink.com>
2017-05-10 21:23:37 -0400

    created hello.js

---
+++ 01/hello.js
@@ -0,0 +1,26 @@
+var x = 'Hello ';
+//declare a variable called x with a value of a string hello world
+console.log(x+'Robert');
+var age = 65;
+
+if(age >= 21) {
+	console.log("You May Legally Have A Drink");
+	if(age < 25) {
+		console.log("You prob want jaegar");
+	};
+	if(age >= 25 && age < 60) {
+		console.log("You prob want an old fashioned");
+	};
+	if (age >= 60) {
+		console.log("you want the drink brandy");
+	};
+	};
+
+	var counter = 1;
+	while(counter < 10) {
+		console.log("woohoo! "+ counter);
+if (counter % 3 == 0) {
+	break;
+};
+counter++;
+	};
